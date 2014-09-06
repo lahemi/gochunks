ef1ede8b8bf1520ce04d38a1c7e2fc0ac32a31df
Live session user <xubuntu@xubuntu.(none)>
2014-09-06 17:18:44 +0000

    Long time no commits, pick up where I left - tidy up IRC output, default to PRIVMSG, handle QUIT.

--- IRCs/clienting/crudeclient.go
+++ IRCs/clienting/crudeclient.go
@@ -4,11 +4,75 @@ import (
 	"bufio"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 )
 
-var writechan = make(chan string, 512)
+var (
+	writechan = make(chan string, 512)
+	linerex   = regexp.MustCompile("^:[^ ]+?!([^ ]+? ){3}:.+")
 
-func doeverything(server, port, nick string) {
+	curTarget string
+
+	irccmdlist = []string{
+		// PRIVMSG,
+		"QUIT",
+		"JOIN",
+		"PART",
+		"NICK",
+		"USER",
+	}
+)
+
+func stdout(s string) {
+	os.Stdout.Write([]byte(s + "\n"))
+}
+func stderr(s string) {
+	os.Stderr.Write([]byte(s + "\n"))
+}
+
+func handleOut(s string) {
+	if linerex.MatchString(s) {
+		spl := strings.SplitN(s, " ", 4)
+		nick := spl[0][1:strings.Index(s, "!")]
+		// cmd := spl[1]
+		target := spl[2]
+		msg := spl[3]
+
+		sep := " | "
+
+		stdout(nick + sep + target + sep + msg)
+
+	} else {
+		stdout(s)
+	}
+}
+
+func handleCmds(s string) bool {
+	cmd := s[:regexp.MustCompile("( |$)").FindStringIndex(s)[0]] // oh my
+	for _, k := range irccmdlist {
+		switch cmd {
+		case "QUIT":
+			writechan <- "QUIT"
+			os.Exit(0)
+		case "JOIN":
+			curTarget = s[5:] // Need a target list.
+			fallthrough
+		case k:
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+
+	var (
+		server = "irc.freenode.net"
+		port   = "6667"
+		nick   = "gecannels"
+	)
+
 	conn, err := net.Dial("tcp", server+":"+port)
 	if err != nil {
 		panic(err)
@@ -21,13 +85,13 @@ func doeverything(server, port, nick string) {
 		for {
 			str, err := r.ReadString(byte('\n'))
 			if err != nil {
-				os.Stderr.Write([]byte("read error\n"))
+				stderr("read error")
 				break
 			}
 			if str[:4] == "PING" {
 				writechan <- "PONG" + str[4:len(str)-2]
 			} else {
-				os.Stdout.Write([]byte(str))
+				handleOut(str[:len(str)-2])
 			}
 		}
 	}()
@@ -35,7 +99,7 @@ func doeverything(server, port, nick string) {
 		for {
 			str := <-writechan
 			if _, err := w.WriteString(str + "\r\n"); err != nil {
-				os.Stderr.Write([]byte("write error\n"))
+				stderr("write error")
 				break
 			}
 			w.Flush()
@@ -48,13 +112,18 @@ func doeverything(server, port, nick string) {
 	for {
 		input, err := in.ReadString('\n')
 		if err != nil {
-			os.Stderr.Write([]byte("error input\n"))
+			stderr("error input")
 			break
 		}
-		writechan <- input[:len(input)-1]
+		inp := input[:len(input)-1]
+		if handleCmds(inp) {
+			writechan <- inp
+		} else {
+			// Default to PRIVMSG
+			writechan <- "PRIVMSG " + curTarget + " :" + inp
+		}
 	}
 }
 
-func main() {
-	doeverything("irc.freenode.net", "6667", "gecannels")
-}
+// Add a target list.
+// Direct IRC output plus user input into files.
